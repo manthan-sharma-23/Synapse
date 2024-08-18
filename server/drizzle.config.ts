32e58799ad67841ed82efec2526383dca4b98abd
Manthan Sharma <manthan2306s@gmail.com>
2024-08-18 21:28:04 +0530

    minor bug

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -1,11 +1,6 @@
 import { defineConfig } from "drizzle-kit";
 
 const dbCredentials = {
-  host: process.env.DB_HOST!,
-  port: Number(process.env.DB_PORT!),
-  user: process.env.DB_USERNAME!,
-  password: process.env.DB_PASSWORD!,
-  database: process.env.DB_NAME!,
   db_url: process.env.DB_URL!,
 };
 
